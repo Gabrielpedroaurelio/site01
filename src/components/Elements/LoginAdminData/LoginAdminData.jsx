fc1741cd6ff4f9bebbeae90244781261dbfd0523
Gabriel Pedro Aurelio <158485269+Gabrielpedroaurelio@users.noreply.github.com>
2025-11-08 10:40:21 -0800

    login administrador terminado a interface grafica

--- src/components/Elements/LoginAdminData/LoginAdminData.jsx
+++ src/components/Elements/LoginAdminData/LoginAdminData.jsx
@@ -1,33 +1,61 @@
+import { useState } from 'react';
 import logotipo from '../../../assets/_images/favicon.png'
-const LoginAdminData=({style})=>{
-  function Login(e){
-    e.preventDefault();
-    // preparar Dados Pra Envio no backend
-    const datas=new FormData(e.target)
-    const request_response=true
- 
-  
+const LoginAdminData = ({ style }) => {
+  const [showTitle, setShowTitle] = useState(false);
+  const obseverTitle = showTitle
+  function ShowTitle(params) {
+    if (obseverTitle) {
+      setShowTitle(false)
+    } else {
+
+      setShowTitle(true)
+    }
 
   }
-return(
+  // Codigo para o login
+  function Login(e) {
+    e.preventDefault();
+    // preparar Dados Pra Envio no backend
+    const datas = new FormData(e.target)
+    const request_response = true
+
+
+
+  }
+  return (
     <>
-  <form action="" onSubmit={Login}>
-    <div className={style.cardImg}>
-        <img src={logotipo} alt="" width="50" />
-    </div>
-  <div className={style.controlerInput}>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="email">E-mail:</label>
-    </div>
-    <div className={style.controlerInput}>
-        <input type="password" name="password" id="password" />
-        <label htmlFor="password">Senha:</label>
-    </div>
-    <div className={style.controlerInput}>
-        <input type="submit" value="Entrar" />
-    </div>
-  </form>
+      <div className={style.form} onLoad={() => {
+        
+          ShowTitle()
+      }}>
+        <div className={style.cardImg}>
+          <img src={logotipo} alt="" width="50" />
+          <span className={`${showTitle ? style.titleLogo : ""}`}>Projecto<span>  ___  </span>LIGA</span>
+        </div>
+        <form action="" onSubmit={Login}>
+
+          <div className={style.controlerInput}>
+            <input type="email" name="email" id="email" />
+            <label htmlFor="email">E-mail:</label>
+          </div>
+          <div className={style.controlerInput}>
+            <input type="password" name="password" id="password" />
+            <label htmlFor="password">Senha:</label>
+          </div>
+          <div className={style.controlerInput}>
+            <input type="submit" value="Entrar" />
+          </div>
+        </form>
+      </div>
+      <div className={style.info}>
+        <div>
+          <h1>Projecto Liga</h1>
+          <p>
+            Painel Administrativo
+          </p>
+        </div>
+      </div>
     </>
-)
+  )
 }
 export default LoginAdminData;
--- src/components/Pages/admin/LoginAdmin/LoginAdmin.jsx
+++ src/components/Pages/admin/LoginAdmin/LoginAdmin.jsx
@@ -1,10 +1,10 @@
 import LoginAdminData from '../../../Elements/LoginAdminData/LoginAdminData'
-import LoginAdminVerify from '../../../Elements/LoginAdminVerify/LoginAdminVerify'
+ 
 import style from './LoginAdmin.module.css'
 const LoginAdmin=()=>{
     const pages_anable=[
         <LoginAdminData style={style}></LoginAdminData>,
-        <LoginAdminVerify style={style}></LoginAdminVerify>
+       
     ]
     return(
         <div className={style.bodyLogin}>
