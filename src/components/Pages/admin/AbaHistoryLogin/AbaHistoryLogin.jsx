fded5ee9e4be676a4e143825aaa55f5acbe3a822
Gabrielpedroaurelio <gabrielpedroaurelio9@gmail.com>
2025-11-10 12:30:34 +0100

    aba history modificada

--- src/components/Pages/admin/AbaHistoryLogin/AbaHistoryLogin.jsx
+++ src/components/Pages/admin/AbaHistoryLogin/AbaHistoryLogin.jsx
@@ -4,33 +4,47 @@ export default function AbaHistoryLogin() {
     const lista = []
     for (let index = 0; index < 100; index++) {
         lista.push(index)
-        
+
     }
     return (
 
         <>
             <div className={style.TableHistoryLogin}>
                 <h3>Historico De Logins</h3>
-                <div className={style.TableHeaderLogin}>
-                    <div>
-                        <span>Código</span>
-                        <span>Usuario</span>
-                        <span>IP</span>
-                        <span>Entrada</span>
-                        <span>Saída</span>
-                    </div>
-                </div>
+
                 <div className={style.TableBodyLogin}>
                     {
                         lista.map((item) => (
 
                             <div key={item}>
-                                <span>{item}</span>
-                                <span>Gabriel</span>
-                                <span>127.0.0.{item}</span>
-                                <span>19-09-2025 21:22:32</span>
-                                <span>19-09-2025 21:22:32</span>
+                                <div className={style.userinfo}>
+                                    <span>
+                                        Gabriel Pedro Aureli
+                                    </span>
+                                    <span>
+                                        gabrielpedroaurelio@gmail.com
 
+                                    </span>
+                                    <span>
+                                        123.65.11.12
+                                    </span>
+                                </div>
+                                <div className={style.hostinfo}>
+                                    <strong>Desktop</strong>
+                                    <strong>Google Chrome</strong>
+
+                                    <button>Rastrear Actividade</button>
+
+                                </div>
+                                <div className={style.sessioninfo}>
+                                    <span>
+                                        Iniciou Sessão:19-09-2025 22:00:02
+                                    </span>
+                                    <span>
+                                        Terminou Sessão:19-09-2025 22:00:02
+                                    </span>
+                                </div>
+                               
                             </div>
                         ))}
                 </div>
